3bb37ecdc96d8b5be6ead0156d2a0d00c525b2a2
Hannyolivia <165098290+Hannyolivia@users.noreply.github.com>
2024-03-31 13:06:22 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 //toogled & responsive
 const navSlide = () => {
-  const burger = document.querySelector(".burger")
-  const navList = document.querySelector(".nav")
-burger.addEventListener("click", () => {
-  navList.classList.toggle("nav-active");
-  burger.classList.toggle("toggle-burger");
+  const burger = document.querySelector(".burger");
+  const navLists = document.querySelector("nav");
 
-});
-
+  burger.addEventListener("click", () => {
+    // Toggle nav list and burger class
+    navLists.classList.toggle("nav-active");
+    burger.classList.toggle("toggle-burger");
+  });
 };
 
 navSlide();
